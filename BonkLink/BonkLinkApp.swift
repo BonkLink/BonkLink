0ef6f42df76f3fad074968f934ba1f6a1d4afbc5
guygreenleaf <guygreenleaf@icloud.com>
2021-04-13 16:13:12 -0700

    Initial Commit

---
+++ BonkLink/BonkLinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonkLinkApp.swift
+//  BonkLink
+//
+//  Created by Guy Greenleaf on 4/13/21.
+//
+
+import SwiftUI
+
+@main
+struct BonkLinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
