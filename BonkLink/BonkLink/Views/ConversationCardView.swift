73e6242f5e5492ea9c0c942fbe5a4069d6096e41
jess-wood <woodjess@sonoma.edu>
2021-04-23 09:51:59 -0700

    ConversationListView done

---
+++ BonkLink/BonkLink/Views/ConversationCardView.swift
@@ -0,0 +1,20 @@
+//
+//  ConversationCardView.swift
+//  BonkLink
+//
+//  Created by Jessica Wood on 4/21/21.
+//
+
+import SwiftUI
+
+struct ConversationCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ConversationCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConversationCardView()
+    }
+}
---
+++ BonkLink/BonkLink/Views/NewConversationView.swift
@@ -0,0 +1,20 @@
+//
+//  NewConversationView.swift
+//  BonkLink
+//
+//  Created by Jessica Wood on 4/21/21.
+//
+
+import SwiftUI
+
+struct NewConversationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NewConversationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NewConversationView()
+    }
+}
